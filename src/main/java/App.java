3ec544e935b9d85edb4f2e007146e37e5c3acc13
andromed <andebobandebob@gmail.com>
2024-11-07 19:06:58 +0300

    2.1.2 Kata решил

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,24 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.getMessage());
+
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat2.getMessage());
+
+        System.out.println(bean == bean2);
+        System.out.println(beanCat == beanCat2);
+
+
+
+
+
     }
 }
